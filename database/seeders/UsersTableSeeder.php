58080ede99a560906b543e69ef770846b1ad97b0
a.delucia <a.delucia@datagrafservizi.com>
2024-02-11 10:55:31 +0100

    Commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+use DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'email' => 'antoninodelucia3@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+            //Agent
+            [
+                'name' => 'Agent',
+                'username' => 'agnet',
+                'email' => 'agent@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'agent',
+                'status' => 'active',
+            ],
+
+            //User
+            [
+                'name' => 'User',
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'user',
+                'status' => 'active',
+            ]
+            ]);
+    }
+}
